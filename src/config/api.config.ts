e77f5f9df6ba7b913c62e223808241751a92a579
Gustavo Bessa <gbvirtual@gmail.com>
2018-05-28 13:13:20 -0300

    Mostrando as Categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080"
+    baseUrl: "http://localhost:8080",
+    bucketBaseURL: "https://s3-sa-east-1.amazonaws.com/gb-curso-spring-ionic"
 }
