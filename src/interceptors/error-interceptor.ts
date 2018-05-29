32c52697d0b76b666db849d6b94a926a1b5d6cca
Gustavo Bessa <gbvirtual@gmail.com>
2018-05-29 10:37:19 -0300

    Tratamento com Alert para outros erros.

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,11 +2,14 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HTTP_INTERCEPTORS, HttpEvent
 import { Observable } from "rxjs/Rx";
 import { Injectable } from "@angular/core";
 import { StorageService } from "../services/storage.service";
+import { AlertCmp, AlertController } from "ionic-angular";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService) {}
+    constructor(
+        public storage: StorageService,
+        public alertCtrl: AlertController) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req)
@@ -23,19 +26,51 @@ export class ErrorInterceptor implements HttpInterceptor {
             console.log(errorObj);
             
             switch(errorObj.status) {
+
+                case 401:
+                this.handle401();
+                break;
+
                 case 403:
                 this.handle403();
                 break;
+
+                default:
+                this.handleDefaultError(errorObj)
             }
             
             return Observable.throw(errorObj);
         }) as any;
     }
+
+    handle401(): any {
+        let alert = this.alertCtrl.create({
+            title: 'Erro 401: Falha de Autenticação',
+            message: 'Email ou senha incorretos',
+            enableBackdropDismiss: false,
+            buttons: [
+                {text: 'Ok'}
+            ]        
+        });
+        alert.present();
+    }
+    
     
     handle403(): any {
         this.storage.setLocalUser(null);
     }
     
+    handleDefaultError(errorObj): any {
+        let alert = this.alertCtrl.create({
+            title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                {text: 'Ok'}
+            ]        
+        });
+        alert.present();
+    }
     
 }
 
