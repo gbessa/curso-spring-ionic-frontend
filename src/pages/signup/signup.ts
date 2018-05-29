2bf31ade2f43c5262b37c384bf1acfd3330accaa
Gustavo Bessa <gbvirtual@gmail.com>
2018-05-29 11:34:22 -0300

    Criando pagina de Signup

---
+++ src/pages/signup/signup.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-signup',
+  templateUrl: 'signup.html',
+})
+export class SignupPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad SignupPage');
+  }
+
+  updateCidades() {
+
+  }
+
+  signupUser() {
+    console.log('Enviou o Form');
+  }
+
+}
