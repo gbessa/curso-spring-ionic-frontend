233d8caac8e1c4a4f43a9674ddafc74c93405dee
Gustavo Bessa <gbvirtual@gmail.com>
2018-06-27 16:20:08 -0300

    Fazendo upload da foto

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -4,13 +4,15 @@ import { API_CONFIG } from "../../config/api.config";
 import { ClienteDTO } from "../../models/cliente.dto";
 import { Observable } from "rxjs/Rx";
 import { StorageService } from "../storage.service";
+import { ImageUtilService } from "../image-util.service";
 
 @Injectable()
 export class ClienteService {
 
     constructor(
         public http: HttpClient,
-        public storage: StorageService) {
+        public storage: StorageService,
+        public imageUtilService: ImageUtilService) {
     }
 
     findById(id: string){
@@ -29,6 +31,18 @@ export class ClienteService {
     insert(obj: ClienteDTO): Observable<any> {
         return this.http.post(`${API_CONFIG.baseUrl}/clientes`,
         obj,
+        {
+            observe: 'response',
+            responseType: 'text'
+        })
+    }
+
+    uploadPicture(picture) {
+        let pictureBlob = this.imageUtilService.dataURItoBlob(picture);
+        let formData: FormData = new FormData();
+        formData.set('file', pictureBlob, 'file.png');
+        return this.http.post(`${API_CONFIG.baseUrl}/clientes/picture`,
+        formData,
         {
             observe: 'response',
             responseType: 'text'
