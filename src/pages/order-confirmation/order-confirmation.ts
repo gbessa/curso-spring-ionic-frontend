6ae8e84e9464b09542e7c56143050bbf0745c49b
Gustavo Bessa <gbvirtual@gmail.com>
2018-06-27 07:29:34 -0300

    Informando pedido registrado ao usuario

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codPedido: string;
 
   constructor(
     public navCtrl: NavController, 
@@ -54,7 +55,7 @@ export class OrderConfirmationPage {
   checkout() {
     this.pedidoService.insert(this.pedido)
     .subscribe(response => {
-      console.log(response.headers.get('location'));
+      this.codPedido = this.extractId(response.headers.get('location'));
       this.cartService.createOrClearCart();
     },
     error => {
@@ -66,5 +67,13 @@ export class OrderConfirmationPage {
 
   back() {
     this.navCtrl.setRoot('CartPage');
+  }
+  home() {
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
+  private extractId(location: string): string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position+1, location.length);
   }
 }
